0f60f5e04703ad351f4676e639d3410139394c17
Szczerybut Konwarszewski <szczerybut.konwarszewski@gmail.com>
2017-05-19 13:37:05 +0200

    --

--- FolderCreator/Form1.Designer.vb
+++ FolderCreator/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class folder_creator
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,143 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(folder_creator))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lb_question = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tBox_folders_per_level = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.tBox_lvls_of_list = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(89, 206)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(101, 44)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Create!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lb_question
+        '
+        Me.lb_question.AutoEllipsis = True
+        Me.lb_question.AutoSize = True
+        Me.lb_question.Font = New System.Drawing.Font("Arial", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.lb_question.Location = New System.Drawing.Point(10, 109)
+        Me.lb_question.Name = "lb_question"
+        Me.lb_question.Size = New System.Drawing.Size(0, 12)
+        Me.lb_question.TabIndex = 1
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem, Me.AboutToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(284, 24)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.MenuToolStripMenuItem.Text = "Menu"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(92, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem1})
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.AboutToolStripMenuItem.Text = "Help"
+        '
+        'AboutToolStripMenuItem1
+        '
+        Me.AboutToolStripMenuItem1.Name = "AboutToolStripMenuItem1"
+        Me.AboutToolStripMenuItem1.Size = New System.Drawing.Size(107, 22)
+        Me.AboutToolStripMenuItem1.Text = "About"
+        '
+        'tBox_folders_per_level
+        '
+        Me.tBox_folders_per_level.Location = New System.Drawing.Point(220, 94)
+        Me.tBox_folders_per_level.Name = "tBox_folders_per_level"
+        Me.tBox_folders_per_level.Size = New System.Drawing.Size(55, 20)
+        Me.tBox_folders_per_level.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 97)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(176, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "How many folders per list? (max. 10)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 124)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(184, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "How many levels of the list? (max. 10)"
+        '
+        'tBox_lvls_of_list
+        '
+        Me.tBox_lvls_of_list.Location = New System.Drawing.Point(220, 121)
+        Me.tBox_lvls_of_list.Name = "tBox_lvls_of_list"
+        Me.tBox_lvls_of_list.Size = New System.Drawing.Size(55, 20)
+        Me.tBox_lvls_of_list.TabIndex = 6
+        '
+        'folder_creator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.tBox_lvls_of_list)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tBox_folders_per_level)
+        Me.Controls.Add(Me.lb_question)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.Name = "folder_creator"
+        Me.Text = "Folder creator"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lb_question As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents tBox_folders_per_level As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents tBox_lvls_of_list As TextBox
 End Class
--- FolderCreator/Form1.vb
+++ FolderCreator/Form1.vb
@@ -1,3 +1,162 @@
-﻿Public Class Form1
+﻿' folder_creator application
+'
+' Project title: folder_creator
+' Author: Szczerybut Konwarszewski
+' Description: Simple application which allows users to create empty folders within folder, may be used as a cipher locker game
+' Date: 17-05-2017
+' 
+
+Public Class folder_creator
+
+    ''''''''''''''' PUBLIC VARIABLES DECLARATION '''''''''''''''
+    Dim tBox_folders_per_level_text As String
+    Dim tBox_folders_per_lvl_number As Integer
+
+    Dim tBox_lvls_of_list_text As String
+    Dim tBox_lvls_of_list_number As Integer
+
+    Dim counter As Integer = tBox_lvls_of_list_number
+    Dim i As Integer
+    Dim final_number_of_lvls = tBox_lvls_of_list_number
+    Dim gCounter As Integer = 1
+
+    Dim alert_Buttons = MessageBoxButtons.YesNo
+    Dim strFileName As String
+
+    ''''''''''''''' PUBLIC FUNCTIONS DECLARATION '''''''''''''''
+
+    ' 2. Checking for input mistakes
+    Function is_tBox_correct(dec_tBox_list_lvl_text)
+
+        ' declare values
+        Dim logResult
+        Dim castInput = dec_tBox_list_lvl_text
+
+        ' check what boolean value should be returned
+        If IsNothing(castInput) Then
+            logResult = True
+            MsgBox("Your query can't be empty")
+        ElseIf Not IsNumeric(castInput) Then
+            logResult = True
+            MsgBox("Your query can't contain string value")
+        ElseIf IsNumeric(castInput) Then
+            logResult = False
+        Else
+            MsgBox("An unknown error has occured. Please, try again")
+        End If
+
+        Return logResult
+    End Function
+
+
+    ' 3. Parsing values
+    Sub parse_folders_per_lvl_values(dec_tBox_list_lvl_text)
+        'Parsing values to numbers
+        tBox_folders_per_lvl_number = Integer.Parse(dec_tBox_list_lvl_text)
+    End Sub
+
+    Sub parse_lvls_of_list_values(dec_tBox_list_lvl_text)
+        'Parsing values to numbers
+        tBox_lvls_of_list_number = Integer.Parse(dec_tBox_list_lvl_text)
+    End Sub
+
+    ' 5. Background for creating directories
+    Public Sub open_Directories(dec_tBox_folders_per_lvl)
+
+        'opening file dialog to specify the path
+
+        Dim fd As FolderBrowserDialog = New FolderBrowserDialog()
+
+        If fd.ShowDialog() = DialogResult.OK Then
+            strFileName = fd.SelectedPath
+        End If
+        fd.ShowNewFolderButton = True
+
+        MsgBox("File path directory you selected is " & strFileName)
+
+        'all directories and its subdirectories are about to be created
+
+        nestedLooping(dec_tBox_folders_per_lvl)
+
+        MsgBox("You succesfully managed to create " & counter & " folders. ",, "Folders HAVE been created")
+    End Sub
+
+    ' 6 Here loops are generated
+    Sub nestedLooping(dec_tBox_folders_per_lvl)
+
+        For i1 As Integer = 1 To dec_tBox_folders_per_lvl
+            For i2 As Integer = 1 To dec_tBox_folders_per_lvl
+                For i3 As Integer = 1 To dec_tBox_folders_per_lvl
+                    For i4 As Integer = 1 To dec_tBox_folders_per_lvl
+                        For i5 As Integer = 1 To dec_tBox_folders_per_lvl
+                            IO.Directory.CreateDirectory(strFileName & "\" & i1 & "\" & i2 & "\" & i3 & "\" & i4 & "\" & i5)
+                            counter += 1
+                        Next i5
+                        IO.Directory.CreateDirectory(strFileName & "\" & i1 & "\" & i2 & "\" & i3 & "\" & i4)
+                        counter += 1
+                    Next i4
+                    IO.Directory.CreateDirectory(strFileName & "\" & i1 & "\" & i2 & "\" & i3)
+                    counter += 1
+                Next i3
+                IO.Directory.CreateDirectory(strFileName & "\" & i1 & "\" & i2)
+                counter += 1
+            Next i2
+            IO.Directory.CreateDirectory(strFileName & "\" & i1)
+            counter += 1
+        Next i1
+    End Sub
+
+    ' 4. Opening file prompt to ensure user that he's/she's aware of what may happen if app is misused
+    Sub when_Tbox_values_are_correct()
+        ' This part of code is going to be executed when above forms are fulfilled
+        Dim alertMsg = "This activity is going to make irrevertible changes in your system (new set of folder is about to be created). Anyway, would you like to continue? If you're sure, click OK. If not, click cancel to avoid unwanted changes"
+
+        Dim response = MsgBox(alertMsg, alert_Buttons, "Would you like to continue?")
+
+        If response = MsgBoxResult.Yes Then
+            ' creating folders stuff
+            open_Directories(tBox_folders_per_lvl_number)
+        Else
+            MsgBox("Unfortunately, folders could not be created",, "Folders HAVE NOT been created")
+        End If
+    End Sub
+
+    ' 1. Submitting entered values
+    Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        ' PRIVATE SUB VARIABLES DECLARATION
+        Dim Tbox1BoolResult = is_tBox_correct(tBox_folders_per_level_text)
+        Dim Tbox2BoolResult = is_tBox_correct(tBox_lvls_of_list_text)
+        ' is_tBox_empty. This one checks
+        If Tbox1BoolResult = True Or Tbox2BoolResult = True Then
+            ' Do-nothing
+        Else
+            parse_folders_per_lvl_values(tBox_folders_per_level_text)
+            parse_lvls_of_list_values(tBox_lvls_of_list_text)
+            when_Tbox_values_are_correct()
+        End If
+
+    End Sub
+
+    ' A. Here you insert folders per level
+    Function tBox_list_levels_TextChanged(sender As Object, e As EventArgs) Handles tBox_folders_per_level.TextChanged
+        ' Grabbing value from a textBox
+        tBox_folders_per_level_text = tBox_folders_per_level.Text
+        Return tBox_folders_per_level_text
+    End Function
+
+    ' B. Here you insert number of levels
+    Function TextBox1_TextChanged(sender As Object, e As EventArgs) Handles tBox_lvls_of_list.TextChanged
+        ' Grabbing value from a textBox
+        tBox_lvls_of_list_text = tBox_lvls_of_list.Text
+        Return tBox_lvls_of_list_text
+    End Function
+
+
+    ''''''''''''''' MAIN FUNCTION '''''''''''''''
+    Public Sub Main()
+
+    End Sub
 
 End Class
+
